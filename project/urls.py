738f8f8a2036439f47adc6a35b6b298d8ab81eed
Artem Petryakov <38184869+NeverDieOne@users.noreply.github.com>
2025-07-10 10:29:14 +0300

    Fix path order

--- project/urls.py
+++ project/urls.py
@@ -4,14 +4,14 @@ from datacenter import views
 
 urlpatterns = [
     path('', views.view_classes, name='classes'),
-    path('<str:year>/<str:letter>/', views.view_class_info,
-        name='class_info'),
-    path('<str:year>/<str:letter>/schedule/',
-        views.view_schedule, name='schedule'),
     path('schoolkid/<str:schoolkid_id>/', views.view_schoolkid,
         name='schoolkid'),
     path(
         'journal/<str:year>/<str:letter>/<str:subject_id>/',
         views.view_journal,
         name='journal'),
+    path('<str:year>/<str:letter>/', views.view_class_info,
+        name='class_info'),
+    path('<str:year>/<str:letter>/schedule/',
+        views.view_schedule, name='schedule'),
 ]
